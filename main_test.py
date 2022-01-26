7f6c3b568e1899f65c3f061bccd355736db4babf
Ben Luijten <w.m.b.luijten@tue.nl>
2022-01-26 14:20:09 +0100

    Added some dummy files and code to use during development, will be removed later

---
+++ main_test.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+import numpy as np
+import tensorflow_ultrasound as tfu
+
+
+
+
+
+
+
+"""
+=============================================================================
+    Initialization
+=============================================================================
+"""
+
+
+from utils.init_GPU import initGPU
+initGPU() #initialize GPU
+
+#load config
+cfg = tfu.dataloader.setup()
+
+
+
+
+"""
+=============================================================================
+    Data loading
+=============================================================================
+"""
+
+ui = tfu.dataloader.DataLoaderUI(cfg)
+
+
+
+
+"""
+=============================================================================
+    Model
+=============================================================================
+"""
+
+
+#create GPU model for training
+model = tfu.models.ABLE.get_model(cfg)
+
+# Create separate cpu model for "full size" inference, to save GPU memory
+with tf.device():
+    cfg_cpu = cfg 
+    #adjust cpu config here
+    model_cpu = tfu.models.ABLE.get_model(cfg_cpu)
+    
+    
+    
+"""
+=============================================================================
+    Callbacks
+=============================================================================
+"""
+    
+
+
+
+"""
+=============================================================================
+    Training
+=============================================================================
+"""
