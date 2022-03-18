cdf3dfd3c33572df86b11fce2277a2ce995513e4
Tristan <t.s.w.stevens@tue.nl>
2022-03-18 11:30:52 +0100

    fixed main script and dataloader now requires x and y directory

--- main.py
+++ main.py
@@ -31,11 +31,12 @@ except ValueError:
 
 RUN_EAGERLY = True # for debugging set to true
 image_shape = (1249, 387)
-epochs = 10
+epochs = 100
 learning_rate = 0.001
 
 # initiate dataloader
 dataloader = DataLoader(
+    destination_folder,
     destination_folder,
     batch_size=1,
     image_shape=image_shape,
@@ -46,13 +47,9 @@ dataloader = DataLoader(
 batch = next(iter(dataloader))
 image = np.squeeze(batch[0])
 
-# get a dataset object for plotting
-dataset = get_dataset(config.data.dataset_name)(config=config.data)
-
 # plot image
-batch = dataloader[0]
-plt.imshow(np.squeeze(batch[0]), cmap='gray')
-
+plt.figure()
+plt.imshow(image, cmap='gray')
 
 model = lista.Unfolding_model(image_shape)
 model.compile(
@@ -64,3 +61,16 @@ model.compile(
 model.summary()
 
 model.fit(dataloader, epochs=epochs)
+
+if len(dataloader) < 10:
+    fig, axs = plt.subplots(len(dataloader), 2)
+    for i, batch in enumerate(dataloader):
+        X, Y = batch
+        out = np.squeeze(model(X))
+        axs[i, 0].imshow(np.squeeze(Y), cmap='gray')
+        axs[i, 1].imshow(out, cmap='gray')
+        
+for ax in axs.ravel():
+    ax.axis('off')
+    
+fig.tight_layout()
