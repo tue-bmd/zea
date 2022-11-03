42eed943641a78938cc070e95226527c4994b7de
Tristan <t.s.w.stevens@tue.nl>
2022-11-03 14:17:29 +0100

    * generate dataset completed for non image data types * extended ui to call generate dataset * envelope detection in processing makes use of probe params * adjusted philips probes to inherent base probe class * filter can be set in probe and that filter will be used in env detection (if not default filter is calculated) * philips dataset more logical loading

--- usbmd/utils/utils.py
+++ usbmd/utils/utils.py
@@ -165,3 +165,18 @@ def to_image(image, value_range: tuple=None, pillow: bool=True):
     if pillow:
         image = Image.fromarray(image)
     return image
+
+def strtobool(val: str):
+    """Convert a string representation of truth to True or False.
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError(f'invalid truth value {val}')
