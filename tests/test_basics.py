a4a321b84eea3ba7afe61e10a117f9e0e5462293
Tristan <t.s.w.stevens@tue.nl>
2022-11-17 10:45:26 +0100

    * tensorflow companding * slight rearrangment of processing units

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -9,19 +9,29 @@ import pytest
 wd = Path(__file__).parent.parent
 sys.path.append(str(wd))
 
-from usbmd.processing import companding
+from usbmd.processing import companding, companding_tf
 
-@pytest.mark.parametrize('comp_type, size', [
-    ('a', (2, 1, 128, 32)),
-    ('a', (512, 512)),
-    ('mu', (2, 1, 128, 32)),
-    ('mu', (512, 512)),
+@pytest.mark.parametrize('comp_type, size, lib', [
+    ('a', (2, 1, 128, 32), 'numpy'),
+    ('a', (512, 512), 'numpy'),
+    ('mu', (2, 1, 128, 32), 'numpy'),
+    ('mu', (512, 512), 'numpy'),
+    ('a', (2, 1, 128, 32), 'tensorflow'),
+    ('a', (512, 512), 'tensorflow'),
+    ('mu', (2, 1, 128, 32), 'tensorflow'),
+    ('mu', (512, 512), 'tensorflow'),
 ])
-def test_companding(comp_type, size):
+def test_companding(comp_type, size, lib):
     """Test companding function"""
     signal = np.clip((np.random.random(size) - 0.5) *2, -1, 1)
+    signal = signal.astype(np.float32)
 
-    signal_out = companding(signal, expand=False, comp_type=comp_type)
-    signal_out = companding(signal_out, expand=True, comp_type=comp_type)
+    if lib == 'tensorflow':
+        compand_func = companding_tf
+    elif lib == 'numpy':
+        compand_func = companding
 
-    return np.testing.assert_almost_equal(signal, signal_out)
+    signal_out = compand_func(signal, expand=False, comp_type=comp_type)
+    signal_out = compand_func(signal_out, expand=True, comp_type=comp_type)
+
+    return np.testing.assert_almost_equal(signal, signal_out, decimal=6)
