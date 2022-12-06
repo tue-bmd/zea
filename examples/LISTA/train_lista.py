c60d209973f4f1fe86779ae6e2d9c96a70044a15
Ben Luijten <w.m.b.luijten@tue.nl>
2022-12-06 17:06:15 +0100

    moved LISTA example

--- examples/LISTA/train_lista.py
+++ examples/LISTA/train_lista.py
@@ -8,7 +8,7 @@ import numpy as np
 import tensorflow as tf
 # make sure you have Pip installed usbmd (see README)
 # import usbmd.tensorflow_ultrasound as usbmd_tf
-from usbmd.tensorflow_ultrasound.dataloader import DataLoader, GenerateDataSet
+from usbmd.tensorflow_ultrasound.dataloader import ImageLoader, GenerateDataSet
 from usbmd.tensorflow_ultrasound.models import lista
 from usbmd.tensorflow_ultrasound.utils.gpu_config import set_gpu_usage
 from usbmd.ui import setup
@@ -38,7 +38,7 @@ epochs = 100
 learning_rate = 0.001
 
 # initiate dataloader
-dataloader = DataLoader(
+dataloader = ImageLoader(
     destination_folder,
     destination_folder,
     batch_size=1,
