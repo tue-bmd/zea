41207a722b83d89784dfcd91d096b4668eac16ca
Tristan Stevens <Tristan.Stevens@philips.com>
2023-01-31 18:43:51 +0000

    merged old check config with new one

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -5,21 +5,19 @@ from pathlib import Path
 
 import yaml
 from schema import SchemaError
+import pytest
 
 wd = Path(__file__).parent.parent
 sys.path.append(str(wd))
 
 from configs.config_validation import config_schema
 
-
-def test_configs():
+@pytest.mark.parametrize('file', list(Path('./configs').glob('*.yaml')))
+def test_config(file):
     """Test if configs are valide according to schema"""
-    files = Path('./configs').glob('*.yaml')
-    for file in files:
-        with open(file, 'r', encoding='utf-8') as f:
-            configuration = yaml.load(f, Loader=yaml.FullLoader)
-        try:
-            print('hoi')
-            config_schema.validate(dict(configuration))
-        except SchemaError as se:
-            raise ValueError(f'Error in config {f}') from se
+    with open(file, 'r', encoding='utf-8') as f:
+        configuration = yaml.load(f, Loader=yaml.FullLoader)
+    try:
+        config_schema.validate(dict(configuration))
+    except SchemaError as se:
+        raise ValueError(f'Error in config {f}') from se
