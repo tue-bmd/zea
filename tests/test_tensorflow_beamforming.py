05778a2044680e9e14e5f6f3de6f43e989d1f6f6
Ben Luijten <w.m.b.luijten@tue.nl>
2023-02-03 16:24:08 +0100

    PR code revisions

--- tests/test_tensorflow_beamforming.py
+++ tests/test_tensorflow_beamforming.py
@@ -1,20 +1,18 @@
 """Test the tf implementation of the beamformers.
 """
-# pylint: skip-file
 import sys
+from pathlib import Path
+
+import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from pathlib import Path
-import pytest
-import cv2
 
+from usbmd.probes import Verasonics_l11_4v
 from usbmd.tensorflow_ultrasound.layers.beamformers import create_beamformer
-from usbmd.probes import get_probe, Verasonics_l11_4v
-from usbmd.datasets import PICMUS
 from usbmd.utils.config import load_config_from_yaml
-from usbmd.utils.pixelgrid import make_pixel_grid, make_pixel_grid_v2
-from usbmd.processing import Process
-from usbmd.utils.simulator import Ultrasound_Simulator
+from usbmd.utils.pixelgrid import make_pixel_grid
+from usbmd.utils.simulator import UltrasoundSimulator
 
 # Add project folder to path to find config files
 wd = Path(__file__).parent.parent
@@ -39,8 +37,7 @@ def test_das_beamforming(debug=False):
     wvln = probe.c/probe.fc
     grid = make_pixel_grid([-19e-3, 19e-3], [0, 63e-3], wvln/4, wvln/4)
 
-    simulator = Ultrasound_Simulator(probe, grid)
-    
+    simulator = UltrasoundSimulator(probe, grid)
     beamformer = create_beamformer(probe, grid, config)
 
     # Ensure reproducible results
@@ -50,29 +47,27 @@ def test_das_beamforming(debug=False):
     # Generate pseudorandom input tensor
     data = simulator.generate(20)
 
-    input = np.expand_dims(data[0], axis=(1,-1))
-    input = np.transpose(input, axes=(0,1,3,2,4))
+    inputs = np.expand_dims(data[0], axis=(1,-1))
+    inputs = np.transpose(inputs, axes=(0,1,3,2,4))
 
     # Perform beamforming and convert to numpy array
-    output = beamformer(input)
+    outputs = beamformer(inputs)
 
 
     # plot results
     if debug:
-        import matplotlib.pyplot as plt
         fig, axs = plt.subplots(1,3)
-
         aspect_ratio = (data[1].shape[1]/data[1].shape[2])/(data[0].shape[1]/data[0].shape[2])
-        axs[0].imshow(np.abs(input.squeeze().T), aspect=aspect_ratio)
+        axs[0].imshow(np.abs(inputs.squeeze().T), aspect=aspect_ratio)
         axs[0].set_title('RF data') 
-        axs[1].imshow(np.squeeze(output))
+        axs[1].imshow(np.squeeze(outputs))
         axs[1].set_title('Beamformed')
         axs[2].imshow(cv2.GaussianBlur(data[1].squeeze(), (5,5), cv2.BORDER_DEFAULT))
         axs[2].set_title('Ground Truth')
         fig.show()
 
     y_true = cv2.GaussianBlur(data[1].squeeze(), (5,5), cv2.BORDER_DEFAULT)
-    y_pred = np.squeeze(output)
+    y_pred = np.squeeze(outputs)
 
     y_true = y_true/y_true.max()
     y_pred = y_pred/y_pred.max()
@@ -84,3 +79,4 @@ def test_das_beamforming(debug=False):
 
 if __name__ == '__main__':
     test_das_beamforming(debug=True)
+    
