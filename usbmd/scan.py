603c7b9a8e1aa429d8c9a80aba403f0125a508a6
Vincent van de Schaft <vincentvdschaft@gmail.com>
2023-02-10 14:16:21 +0100

    Input arguments and attributes scan class changed

--- usbmd/scan.py
+++ usbmd/scan.py
@@ -31,10 +31,12 @@ def initialize_scan_from_config(config):
 class Scan:
     def __init__(self, N_tx=75, xlims=(-0.01, 0.01), ylims=(0, 0),
                  zlims=(0, 0.04), fc=7e6, fs=28e6, c=1540, modtype='rf',
-                 N_ax=256, N_lat=128):
+                 N_ax=3328, N_rec_el=128, Nx=128, Nz=128):
         """
         Initializes a Scan object representing the number and type of transmits,
         and the target pixels to beamform to.
+
+        The Scan object generates a pixel grid when it is initialized.
 
         Args:
             probe (Probe, str, optional): Probe object to read values from or
@@ -54,35 +56,41 @@ class Scan:
             c (float, optional): The speed of sound in m/s. Defaults to 1540.
             modtype(string, optional): The modulation type. ('rf' or 'iq').
             Defaults to 'rf'
-            N_ax (int, optional): The number of pixels in the axial direction.
+            N_ax (int, optional): The number of samples per in a receive recording per channel.
             Defaults to None.
-            N_lat (int, optional): The number of pixels in the lateral
-            direction. Defaults to None.
+            N_x (int, optional): The number of pixels in the lateral direction in the beamforming grid.
+            Defaults to None.
+            N_z (int, optional): The number of pixels in the axial direction in the beamforming grid.
+            Defaults to None.
 
         Raises:
             NotImplementedError: Initializing from probe not yet implemented.
         """
         assert modtype in ['rf', 'iq'], "modtype must be either 'rf' or 'iq'."
 
+        # Attributes concerning channel data
         self.N_tx = N_tx
-        self.xlims = xlims
-        self.ylims = ylims
-        self.zlims = zlims
         self.fc = fc
         self.fs = fs
         self.c = c
         self.modtype = modtype
         self.N_ax = N_ax
-        self.N_lat = N_lat
+        self.N_rec_el = N_rec_el
         self.time_zero = np.zeros((N_tx,))
         self.fdemod = self.fc if modtype == 'iq' else 0.
+        self.N_ch = 2 if modtype == 'iq' else 1
         self.wvln = self.c / self.fc
+
+        # Beamforming grid related attributes
+        self.xlims = xlims
+        self.ylims = ylims
+        self.zlims = zlims
         self.grid = None
 
         # !!! TODO, implement this such that no aliasing occurs
         if self.fdemod == 0:
             self.grid = make_pixel_grid_v2(
-                self.xlims, self.zlims, self.N_lat, self.N_ax)
+                self.xlims, self.zlims, Nx, Nz)
         else:
             pixels_per_wavelength = 3
             dx = self.wvln / pixels_per_wavelength
@@ -96,10 +104,11 @@ class FocussedScan(Scan):
     origin, angle, and focus defined.
     """
 
-    def __init__(self, probe=None, N_tx=75, xlim=(-0.01, 0.01), ylim=(0, 0),
-                 zlim=(0, 0.04), fc=7e6, fs=28e6, N_ax=None, N_lat=None, origins=None,
-                 focus_distances=None, angles=None):
-        super().__init__(probe, N_tx, xlim, ylim, zlim, fc, fs, N_ax, N_lat)
+    def __init__(self, N_tx=75, xlims=(-0.01, 0.01), ylims=(0, 0),
+                 zlims=(0, 0.04), fc=7e6, fs=28e6, c=1540, modtype='rf',
+                 N_ax=256, N_lat=128, origins=None,focus_distances=None,
+                 angles=None, Nx=128, Nz=128):
+        super().__init__(N_tx, xlims, ylims, zlims, fc, fs, c, modtype, N_ax, Nx, Nz)
 
         self.origins = origins
         self.focus_distances = focus_distances
@@ -111,19 +120,21 @@ class PlaneWaveScan(Scan):
     Class representing a plane wave scan where every transmit has an angle.
     """
 
-    def __init__(self, probe=None, N_tx=75, xlim=(-0.01, 0.01), ylim=(0, 0),
-                 zlim=(0, 0.04), fc=7e6, fs=28e6, N_ax=None, N_lat=None, angles=None):
+    def __init__(self, N_tx=75, xlims=(-0.01, 0.01), ylims=(0, 0),
+                 zlims=(0, 0.04), fc=7e6, fs=28e6, c=1540, modtype='rf',
+                 N_ax=256, Nx=128, Nz=128, angles=None):
 
-        super().__init__(probe, N_tx, xlim, ylim, zlim, fc, fs, N_ax, N_lat)
-
+        super().__init__(N_tx, xlims, ylims, zlims, fc, fs, c, modtype, N_ax, Nx, Nz)
+        assert angles is not None
         self.angles = angles
 
 
 class CircularWaveScan(Scan):
-    def __init__(self, probe=None, N_tx=75, xlim=(-0.01, 0.01), ylim=(0, 0),
-                 zlim=(0, 0.04), fc=7e6, fs=28e6, N_ax=None, N_lat=None, focus=None):
+    def __init__(self, N_tx=75, xlims=(-0.01, 0.01), ylims=(0, 0),
+                 zlims=(0, 0.04), fc=7e6, fs=28e6, c=1540, modtype='rf',
+                 N_ax=256, Nx=128, Nz=128, focus=None):
 
-        super().__init__(probe, N_tx, xlim, ylim, zlim, fc, fs, N_ax, N_lat)
+        super().__init__(N_tx, xlims, ylims, zlims, fc, fs, c, modtype, N_ax, Nx, Nz)
         raise NotImplementedError('CircularWaveScan has not been implemented.')
 
 
