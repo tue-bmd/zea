4873e5616e2751f196ee54cb1e3af2acf755c316
Ben Luijten <w.m.b.luijten@tue.nl>
2023-02-16 17:41:38 +0100

    cleanup

--- tests/test_pytorch_beamforming.py
+++ tests/test_pytorch_beamforming.py
@@ -20,47 +20,6 @@ from usbmd.utils.simulator import UltrasoundSimulator
 wd = Path(__file__).parent.parent
 sys.path.append(str(wd))
 
-# def test_das_beamforming():
-#     """
-#     Performs DAS beamforming on random data to verify that no errors occur. Does
-#     not check correctness of the output.
-#     """
-
-#     config = load_config_from_yaml(r'./tests/config_test.yaml')
-
-#     # Ensure DAS beamforming even if the config were to change
-#     config.model.type = 'das'
-#     config.data.dataset_name = 'picmus'
-#     config.data.n_angles = 1
-
-#     #probe = get_probe(config)
-#     probe = Verasonics_l11_4v(config)
-
-#     # Perform the beamforming on a small grid to ensure the test runs quickly
-#     grid = make_pixel_grid_v2(
-#         config.scan.xlims,
-#         config.scan.zlims,
-#         config.get('Nx', 64),
-#         config.get('Nz', 32))
-
-#     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-
-#     beamformer = create_beamformer(probe, grid, config)
-
-#     # Ensure reproducible results
-#     torch.random.manual_seed(0)
-
-#     dataset = DummyDataset()
-
-#     # Get dummy data from dataset and add batch dimension
-#     input_data = dataset[0][None]
-
-#     # Perform beamforming on device
-#     on_device_torch(beamformer, input_data, device=device, return_numpy=True)
-
-
-
-
 def test_das_beamforming(debug=False, compare_gt=True):
     """Performs DAS beamforming on random data to verify that no errors occur. Does
     not check correctness of the output.
