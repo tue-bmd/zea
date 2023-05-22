1fb750aacbcfae58ed97cc5d29e5837907043b51
Ben Luijten <w.m.b.luijten@tue.nl>
2023-05-22 21:29:59 +0200

    Pylint...

--- usbmd/verasonics/webserver/demo_setup.py
+++ usbmd/verasonics/webserver/demo_setup.py
@@ -120,6 +120,7 @@ def create_DAS(n_angles):
         print('Could not compile model, running uncompiled')
 
     # Build model by passing a dictionary of dummy data with correct dtype and shape
+    # pylint disable=not-an-iterable
     dummy_input = {
         inp.name.strip('_input'): tf.zeros(inp.shape, dtype=inp.dtype) for inp in model.inputs
     }
