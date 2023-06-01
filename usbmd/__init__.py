ea8f80ae2ed3dc278c6b8de8c8b9cdcc31c68ba2
Ben Luijten <w.m.b.luijten@tue.nl>
2023-06-01 17:41:33 +0200

    Moved beamformer registry import to top level init such that they are registered before scripts are started

--- usbmd/__init__.py
+++ usbmd/__init__.py
@@ -4,3 +4,10 @@ The official documentation for the Python package `usbmd` - a convenient ultraso
 .. include:: ./README.md
 """
 from . __version__ import __version__
+
+# pylint: disable=unused-import
+# Register beamforing types in registry
+from usbmd.tensorflow_ultrasound.layers import unfolded_bf
+from usbmd.tensorflow_ultrasound.layers import minimum_variance
+from usbmd.tensorflow_ultrasound.layers import beamformers
+from usbmd.tensorflow_ultrasound.layers import random_minimum
