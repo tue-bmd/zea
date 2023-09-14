bb43e1e7cf391448569df0ac32c3317484dae3cf
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2023-09-14 10:26:34 +0200

    Swap two lines to fix pip install

--- usbmd/__init__.py
+++ usbmd/__init__.py
@@ -5,7 +5,7 @@ The official documentation for the Python package `usbmd` - a convenient ultraso
 """
 # pylint: disable=unused-import
 # Register beamforing types in registry
+from .__version__ import __version__
+
 from usbmd.tensorflow_ultrasound.layers import (beamformers, minimum_variance,
                                                 random_minimum, unfolded_bf)
-
-from .__version__ import __version__
