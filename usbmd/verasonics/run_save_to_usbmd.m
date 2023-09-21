b0ec2a26436b7f9272f39e63a3b8009fdafbb56e
Vincent van de Schaft <vincentvdschaft@gmail.com>
2023-09-21 10:52:55 +0200

    Asks for output dir and filename Use ones from workspace if present

--- usbmd/verasonics/run_save_to_usbmd.m
+++ usbmd/verasonics/run_save_to_usbmd.m
@@ -5,15 +5,32 @@
 % The dataset can later be read using the usbmd toolbox. Just run
 % this script after acquisition in the same workspace in Matlab.
 %
-% Note that the dimensions of the arrays are all in reversed order. This is
-% needed because MATLAB stores data in column-major order, while numpy
-% works with row-major order. When viewed in numpy the order will be
-% correct.
+% If output_dir is defined in the workspace the script will save to this
+% directory.
+%
+% If filename is defined in the workspace the script will save with this
+% filename.
+%
+% The script will generate a unique filename if the output directory by
+% appending a number to the filename.
 %
 % The script assumes that every frame has the same transmit and receive
 % events.
 
 % Define the output directory
+if exist('output_dir', 'var') && exist(output_dir, 'dir')
+    disp('output_dir already defined in workspace')
+else
+    output_dir = input('Output directory: ', 's');
+end
+
+if exist('filename', 'var') && exist(filename, 'dir')
+    disp('filename already defined in workspace');
+else
+    % Prompt the user for the filename
+    filename = input('Enter the filename (without extension): ', 's');
+end
+
 output_dir = 'D:\Vincent_van_de_schaft\verasonics';
 
 % Prompt the user for the filename
