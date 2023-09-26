4c46fd4f31728b2f6183f6db7ed446eb346820d3
Tristan <t.s.w.stevens@tue.nl>
2023-09-26 13:23:40 +0200

    better tests and added GPUs more than one constraint

--- tests/test_gpu_usage.py
+++ tests/test_gpu_usage.py
@@ -8,11 +8,20 @@ from usbmd.tensorflow_ultrasound.utils.gpu_config import \
     set_gpu_usage as set_gpu_usage_tf
 
 
-@pytest.mark.parametrize('device', ['cpu', 'gpu:0', 'auto:-1', 'cuda:0'])
-def test_gpu_usage(device):
+@pytest.mark.parametrize('device, ml_lib', [
+    ('cpu', 'tensorflow'),
+    ('cpu', 'torch'),
+    ('gpu:0', 'tensorflow'),
+    ('gpu:0', 'torch'),
+    ('cuda:0', 'tensorflow'),
+    ('cuda:0', 'torch'),
+    ('auto:-1', 'tensorflow'),
+    ('auto:-1', 'torch'),
+])
+def test_gpu_usage(device, ml_lib):
     """Test gpu usage setting script"""
     # this only works when this tests runs first, since gpu usage should be done first
-    # set_gpu_usage()
-    set_gpu_usage_tf(device=device)
-
-    set_gpu_usage_pytorch(device=device)
+    if ml_lib == 'tensorflow':
+        set_gpu_usage_tf(device=device)
+    elif ml_lib == 'torch':
+        set_gpu_usage_pytorch(device=device)
