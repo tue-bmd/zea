53cc5d34b38c3f90088c203f0ddf4ff7678307da
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2023-09-26 09:28:14 +0200

    Add fallback when nvidia-smi is not available

--- usbmd/utils/gpu_utils.py
+++ usbmd/utils/gpu_utils.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess as sp
+import warnings
 import pandas as pd
 
 def get_gpu_memory(verbose=True):
@@ -17,7 +18,12 @@ def get_gpu_memory(verbose=True):
         return x.decode('ascii').split('\n')[:-1]
 
     COMMAND = "nvidia-smi --query-gpu=memory.free --format=csv"
-    memory_free_info = _output_to_list(sp.check_output(COMMAND.split()))[1:]
+    try:
+        memory_free_info = _output_to_list(sp.check_output(COMMAND.split()))[1:]
+    except:
+        warnings.warn('Could not run nvidia-smi. Assuming there is no GPU.')
+        memory_free_info = []
+
     memory_free_values = [int(x.split()[0])
                           for i, x in enumerate(memory_free_info)]
 
