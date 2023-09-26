352de48c21af6b5bd947738547f42e8a544e77d1
Tristan <t.s.w.stevens@tue.nl>
2023-09-26 13:00:10 +0200

    added gpu tests and extra assertion

--- tests/test_gpu_usage.py
+++ tests/test_gpu_usage.py
@@ -1,11 +1,18 @@
 """GPU usage testing
 """
-# from usbmd.tensorflow_ultrasound.utils.gpu_config import set_gpu_usage
+import pytest
+
+from usbmd.pytorch_ultrasound.utils.gpu_config import \
+    set_gpu_usage as set_gpu_usage_pytorch
+from usbmd.tensorflow_ultrasound.utils.gpu_config import \
+    set_gpu_usage as set_gpu_usage_tf
 
 
-def test_gpu_usage():
+@pytest.mark.parametrize('device', ['cpu', 'gpu:0', 'auto:-1', 'cuda:0'])
+def test_gpu_usage(device):
     """Test gpu usage setting script"""
     # this only works when this tests runs first, since gpu usage should be done first
     # set_gpu_usage()
-    # set_gpu_usage(device='cpu')
-    return
+    set_gpu_usage_tf(device=device)
+
+    set_gpu_usage_pytorch(device=device)
