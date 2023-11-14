ba3185c3ff93d9deacfed76a76cd951205fcfebd
Tristan <t.s.w.stevens@tue.nl>
2023-11-14 12:57:41 +0100

    changed setup use in camus * swapped setup with setup_config in setup_usbmd * changed file to config_path for consistency

--- usbmd/setup_usbmd.py
+++ usbmd/setup_usbmd.py
@@ -19,45 +19,6 @@ from usbmd.utils.git_info import get_git_summary
 from usbmd.utils.io_lib import filename_from_window_dialog
 
 
-def setup_config(file=None):
-    """Setup function for config. Retrieves config file and checks for validity.
-
-    Args:
-        file (str, optional): file path to config yaml. Defaults to None.
-            if None, argparser is checked. If that is None as well, the window
-            ui will pop up for choosing the config file manually.
-
-    Returns:
-        config (dict): config object / dict.
-
-    """
-    if file is None:
-        # if no argument is provided resort to UI window
-        filetype = "yaml"
-        try:
-            file = filename_from_window_dialog(
-                f"Choose .{filetype} file",
-                filetypes=((filetype, "*." + filetype),),
-                initialdir="./configs",
-            )
-        except Exception as e:
-            raise ValueError(
-                "Please specify the path to a config file through --config flag "
-                "if GUI is not working (usually on headless servers)."
-            ) from e
-
-    config = load_config_from_yaml(Path(file))
-    print(f"Using config file: {file}")
-    config = check_config(config)
-
-    ## git
-    cwd = Path.cwd().stem
-    if cwd in ("Ultrasound-BMd", "usbmd"):
-        config["git"] = get_git_summary()
-
-    return config
-
-
 def setup(
     config_path: str = None,
     user_config: Union[str, dict] = None,
@@ -83,6 +44,45 @@ def setup(
 
     # Init GPU / CPU according to config
     config.device = init_device(config.ml_library, config.device)
+
+    return config
+
+
+def setup_config(config_path: str = None):
+    """Setup function for config. Retrieves config file and checks for validity.
+
+    Args:
+        config_path (str, optional): file path to config yaml. Defaults to None.
+            if None, argparser is checked. If that is None as well, the window
+            ui will pop up for choosing the config file manually.
+
+    Returns:
+        config (dict): config object / dict.
+
+    """
+    if config_path is None:
+        # if no argument is provided resort to UI window
+        filetype = "yaml"
+        try:
+            file = filename_from_window_dialog(
+                f"Choose .{filetype} file",
+                filetypes=((filetype, "*." + filetype),),
+                initialdir="./configs",
+            )
+        except Exception as e:
+            raise ValueError(
+                "Please specify the path to a config file through --config flag "
+                "if GUI is not working (usually on headless servers)."
+            ) from e
+
+    config = load_config_from_yaml(Path(config_path))
+    print(f"Using config file: {config_path}")
+    config = check_config(config)
+
+    ## git
+    cwd = Path.cwd().stem
+    if cwd in ("Ultrasound-BMd", "usbmd"):
+        config["git"] = get_git_summary()
 
     return config
 
