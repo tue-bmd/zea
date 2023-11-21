a3ed800c7e044c51d74817477c857d92e9e2490c
Tristan <t.s.w.stevens@tue.nl>
2023-11-21 13:11:24 +0100

    mistakenly put camus instead of probes in pytest.skip

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -41,7 +41,7 @@ config_initializers = [
 )
 def test_all_configs_valid(file):
     """Test if configs are valide according to schema"""
-    if file.name == "config_camus.yaml":
+    if file.name == "probes.yaml":
         pytest.skip("probes.yaml is not checked here.")
     with open(file, "r", encoding="utf-8") as f:
         configuration = yaml.load(f, Loader=yaml.FullLoader)
