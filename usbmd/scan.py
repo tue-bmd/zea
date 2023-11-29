8950b18ff5f20eee15bbf00f5e13252a4df7431c
BeatriceFederici <b.federici@tue.nl>
2023-11-29 11:30:46 +0100

    pylint changes required

--- usbmd/scan.py
+++ usbmd/scan.py
@@ -101,8 +101,8 @@ class Scan:
                 to None, then all transmits are used. Defaults to None.
             probe_geometry (np.ndarray, optional): (n_el, 3) array with element positions
                 in meters. Necessary for automatic xlim calculation if not set. Defaults to None.
-            time_to_next_transmit (np.ndarray, float, optional): The time between subsequent transmit
-            events of shape (n_tx*n_frames,). Defaults to None.
+            time_to_next_transmit (np.ndarray, float, optional): The time between subsequent
+                transmit events of shape (n_tx*n_frames,). Defaults to None.
 
         Raises:
             NotImplementedError: Initializing from probe not yet implemented.
