c682394544cf5c70ee532a7c9931f0f0b9c5038f
Ben Luijten <w.m.b.luijten@tue.nl>
2023-12-04 14:55:02 +0100

    pylint

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,9 +1,12 @@
+""" Module for testing loss functions """
+
 import numpy as np
 
 from usbmd.tensorflow_ultrasound.losses import SMSLE
 
 
 def test_smsle():
+    """Test SMSLE loss function"""
     # Create random y_true and y_pred data
     y_true = np.random.rand(1, 11, 128, 512, 2).astype(np.float32)
     y_pred = np.random.rand(1, 11, 128, 512, 2).astype(np.float32)
@@ -14,6 +17,7 @@ def test_smsle():
 
     # Check if loss is a scalar
     assert loss.shape == ()
+
 
 if __name__ == "__main__":
     test_smsle()
