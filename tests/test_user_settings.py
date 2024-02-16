97164dbe99a327c22d2946ce98b68c4908d4a854
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2024-02-16 14:09:32 +0100

    Fix previous

--- tests/test_user_settings.py
+++ tests/test_user_settings.py
@@ -1,11 +1,16 @@
 """User settings testing
 """
+
+import pytest
+
 from usbmd.common import set_data_paths
 
 
-def test_set_data_paths():
+def test_set_data_paths(monkeypatch):
     """Test set data paths"""
-    set_data_paths()
+    # Will raise OSError because input() cannot be called in test
+    with pytest.raises(OSError):
+        set_data_paths()
     set_data_paths(user_config="users.yaml", local=False)
     set_data_paths(user_config="users.yaml", local=True)
     user_config = {
