2c38cc0a9dce4d130db6ad902de9c929c591fed1
Vincent van de Schaft <vincentvdschaft@gmail.com>
2024-03-07 10:32:02 +0100

    Pylint

--- usbmd/ui.py
+++ usbmd/ui.py
@@ -22,7 +22,6 @@ from PIL import Image
 wd = Path(__file__).parent.resolve()
 sys.path.append(str(wd))
 
-import usbmd.utils.log as log
 from usbmd.datasets import get_dataset
 from usbmd.display import to_8bit
 from usbmd.generate import GenerateDataSet
@@ -30,6 +29,7 @@ from usbmd.probes import get_probe
 from usbmd.processing import Process
 from usbmd.setup_usbmd import setup
 from usbmd.usbmd_gui import USBMDApp
+from usbmd.utils import log
 from usbmd.utils.checks import _DATA_TYPES
 from usbmd.utils.config import Config
 from usbmd.utils.io_lib import (
