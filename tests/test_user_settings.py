7e3e510352ba242e414838c76b70e72d800a919b
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2024-04-11 11:31:57 +0200

    Do not allow arbitrary keys but you can set output key (and fix tests)

--- tests/test_user_settings.py
+++ tests/test_user_settings.py
@@ -1,19 +1,46 @@
 """User settings testing
 """
 
+import getpass
+import socket
+from pathlib import Path
+
 import pytest
+import yaml
+from ipykernel.kernelbase import StdinNotImplementedError
 
-from usbmd.common import set_data_paths
+from usbmd.common import DEFAULT_USER, DEFAULT_USERS_CONFIG_PATH, set_data_paths
 
 
-def test_set_data_paths(monkeypatch):
+def test_set_data_paths():
     """Test set data paths"""
-    # Will raise OSError because input() cannot be called in test
-    with pytest.raises(OSError):
-        set_data_paths()
-    set_data_paths(user_config="users.yaml", local=False)
-    set_data_paths(user_config="users.yaml", local=True)
+
+    # Create default users.yaml because test cannot handle stdin in create_new_user()
+    if not Path(DEFAULT_USERS_CONFIG_PATH).is_file():
+        with open(DEFAULT_USERS_CONFIG_PATH, "w", encoding="utf-8") as file:
+            yaml.dump(DEFAULT_USER, file, default_flow_style=False)
+
+    # Test set_data_paths
+    set_data_paths(local=True)
+    set_data_paths(local=False)
+
+    # Test with custom user_config
     user_config = {
-        "myusername": {"hostname": {"data_root": "C:/path_to_my_data_root/"}}
+        getpass.getuser(): {
+            socket.gethostname(): {
+                "data_root": "C:/path_to_my_data_root/",
+                "output": {
+                    "local": "C:/path_to_my_output/",
+                    "remote": "Z:/path_to_my_output/",
+                },
+            }
+        }
     }
     set_data_paths(user_config)
+    data_paths = set_data_paths(user_config, local=False)
+    assert "data_root" in data_paths
+    assert "output" in data_paths
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", __file__])
