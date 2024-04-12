ce2509235d058014ce9354c1fcb28cc69b6f3757
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2024-04-12 17:28:16 +0200

    Add test that covers the bug from before

--- tests/test_user_settings.py
+++ tests/test_user_settings.py
@@ -15,7 +15,7 @@ def test_set_data_paths():
 
     user_config_path = "users.test.yaml"
 
-    # Test set_data_paths
+    # Test set_data_paths with default user_config
     set_data_paths(user_config_path, local=True)
     set_data_paths(user_config_path, local=False)
 
@@ -35,6 +35,22 @@ def test_set_data_paths():
     data_paths = set_data_paths(user_config, local=False)
     assert "data_root" in data_paths
     assert "output" in data_paths
+
+    # Test with custom user_config that does not have the optional output key
+    user_config = {
+        getpass.getuser(): {
+            socket.gethostname(): {
+                "data_root": {
+                    "local": "C:/path_to_my_output/",
+                    "remote": "Z:/path_to_my_output/",
+                },
+            }
+        }
+    }
+    data_paths = set_data_paths(user_config)
+    assert "data_root" in data_paths
+    data_paths = set_data_paths(user_config, local=False)
+    assert "data_root" in data_paths
 
     # clean up
     Path(user_config_path).unlink()
