11a243ce2318b8be3aecb0314a098a7c955bef14
OisinNolan <oinolan@tcd.ie>
2024-06-20 11:34:09 +0200

    Add sector reweighting util to metrics

--- tests/test_losses_and_metrics.py
+++ tests/test_losses_and_metrics.py
@@ -8,7 +8,7 @@ import pytest
 import usbmd
 from usbmd.backend.tensorflow.losses import SMSLE
 from usbmd.registry import metrics_registry
-from usbmd.utils.metrics import get_metric
+from usbmd.utils.metrics import get_metric, sector_reweight_image
 
 
 def test_smsle():
@@ -74,3 +74,18 @@ def test_metrics_registry():
         if metric == "get_metric":
             continue
         assert metric in metrics_registry, f"{metric} is not in the metrics registry"
+
+def test_sector_reweight_image():
+    # arrange
+    cube_of_ones = np.ones((3, 3, 3))
+    
+    # act
+    reweighted_cube = sector_reweight_image(cube_of_ones, 180)
+    
+    # assert
+    # depths are set at the 'center' of each pixel index
+    expected_depths = np.array([0.5, 1.5, 2.5])
+    expected_reweighting_per_depth = np.pi # (180 / 360) * 2 * pi = pi
+    expected_result = cube_of_ones * expected_depths[:, None] * expected_reweighting_per_depth
+    assert np.all(expected_result == reweighted_cube)
+    
