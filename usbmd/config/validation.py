d6eb79af03dc71a7599387150a97e27dc9216a65
Tristan Stevens <t.s.w.stevens@tue.nl>
2024-07-24 16:44:59 +0200

    black + log.info instead of print

--- usbmd/config/validation.py
+++ usbmd/config/validation.py
@@ -84,7 +84,7 @@ model_schema = Schema(
             Optional("patches", default=1): positive_integer,
             Optional("jit", default=False): bool,
             Optional("auto_pressure_weighting", default=False): bool,
-            Optional("auto_pressure_weighting_savefigs",default=False):bool,
+            Optional("auto_pressure_weighting_savefigs", default=False): bool,
         },
     }
 )
