ed7386c942e5d26389cc9abb32dcac788a373846
Tristan Stevens <t.s.w.stevens@tue.nl>
2024-07-26 00:10:22 +0200

    fixed matlab and verasonics saving for event data

--- usbmd/data/read_h5.py
+++ usbmd/data/read_h5.py
@@ -182,7 +182,7 @@ def recursively_load_dict_contents_from_group(
                         ans[key] = float(ans[key])
         elif isinstance(item, h5py._hl.group.Group):
             ans[key] = recursively_load_dict_contents_from_group(
-                h5file, path + key + "/"
+                h5file, path + "/" + key + "/"
             )
     return ans
 
