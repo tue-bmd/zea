168402967288b7eb55541c38188b534d4fc0e1fd
Tristan Stevens <t.s.w.stevens@tue.nl>
2024-08-02 13:33:53 +0000

    bumping version

--- usbmd/__init__.py
+++ usbmd/__init__.py
@@ -4,11 +4,11 @@ The official documentation for the Python package `usbmd` - a convenient ultraso
 .. include:: ./README.md
 """
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 # Main (isort: split)
 from .config import Config, load_config_from_yaml
+from .datapaths import set_data_paths
 from .setup_usbmd import setup, setup_config
 from .utils import log
 from .utils.device import init_device
-from .datapaths import set_data_paths
