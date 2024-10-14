fdae535f5c53b66bcbafc19d28c82c6601f2cd41
Vincent van de Schaft <vincentvdschaft@gmail.com>
2024-10-14 11:45:48 +0200

    Add safe initialization of scan

--- usbmd/data/data_format.py
+++ usbmd/data/data_format.py
@@ -12,7 +12,12 @@ import numpy as np
 from usbmd.data.read_h5 import recursively_load_dict_contents_from_group
 from usbmd.probes import get_probe
 from usbmd.scan import Scan, cast_scan_parameters
-from usbmd.utils import first_not_none_item, log, update_dictionary
+from usbmd.utils import (
+    first_not_none_item,
+    log,
+    update_dictionary,
+    safe_initialize_class,
+)
 from usbmd.utils.checks import _DATA_TYPES, validate_dataset
 
 
@@ -835,15 +840,7 @@ def load_usbmd_file(
         # merge file scan parameters with config scan parameters
         scan_params = update_dictionary(file_scan_parameters, config_scan_dict)
 
-        # Retrieve the argument names of the Scan class
-        sig = inspect.signature(Scan.__init__)
-
-        # Filter out the arguments that are not part of the Scan class
-        scan_params = {
-            key: scan_params[key] for key in sig.parameters if key in scan_params
-        }
-
         # Initialize the scan object
-        scan = Scan(**scan_params)
+        scan = safe_initialize_class(Scan, **scan_params)
 
         return data, scan, probe
