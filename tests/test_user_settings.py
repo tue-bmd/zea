27b1cb3db6e4eb016b584def6bfb6a51660f747b
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2024-10-29 14:37:51 +0000

    Refactor test

--- tests/test_user_settings.py
+++ tests/test_user_settings.py
@@ -3,57 +3,49 @@
 
 import getpass
 import socket
-from pathlib import Path
 
 import pytest
 
 from usbmd.datapaths import set_data_paths
 
+user_config1 = {
+    getpass.getuser(): {
+        socket.gethostname(): {
+            "data_root": "C:/path_to_my_data_root/",
+            "output": {
+                "local": "C:/path_to_my_output/",
+                "remote": "Z:/path_to_my_output/",
+            },
+        }
+    }
+}
 
-def test_set_data_paths():
+user_config2 = {
+    getpass.getuser(): {
+        socket.gethostname(): {
+            "data_root": {
+                "local": "C:/path_to_my_output/",
+                "remote": "Z:/path_to_my_output/",
+            },
+        }
+    }
+}
+
+
+@pytest.mark.parametrize("user_config", [user_config1, user_config2, "users.test.yaml"])
+def test_set_data_paths(tmp_path, user_config):
     """Test set data paths"""
 
-    user_config_path = "users.test.yaml"
+    if isinstance(user_config, str):
+        # Add temp path and set as string
+        user_config = str(tmp_path / user_config)
 
-    # Test set_data_paths with default user_config
-    set_data_paths(user_config_path, local=True)
-    set_data_paths(user_config_path, local=False)
-
-    # Test with custom user_config
-    user_config = {
-        getpass.getuser(): {
-            socket.gethostname(): {
-                "data_root": "C:/path_to_my_data_root/",
-                "output": {
-                    "local": "C:/path_to_my_output/",
-                    "remote": "Z:/path_to_my_output/",
-                },
-            }
-        }
-    }
-    set_data_paths(user_config)
-    data_paths = set_data_paths(user_config, local=False)
-    assert "data_root" in data_paths
-    assert "output" in data_paths
-
-    # Test with custom user_config that does not have the optional output key
-    user_config = {
-        getpass.getuser(): {
-            socket.gethostname(): {
-                "data_root": {
-                    "local": "C:/path_to_my_output/",
-                    "remote": "Z:/path_to_my_output/",
-                },
-            }
-        }
-    }
-    data_paths = set_data_paths(user_config)
-    assert "data_root" in data_paths
-    data_paths = set_data_paths(user_config, local=False)
-    assert "data_root" in data_paths
-
-    # clean up
-    Path(user_config_path).unlink()
+    for local in [True, False]:
+        data_paths = set_data_paths(user_config, local=local)
+        assert (
+            "data_root" in data_paths
+        ), f"data_root not in data_paths for local={local}"
+        assert "output" in data_paths, f"output not in data_paths for local={local}"
 
 
 if __name__ == "__main__":
