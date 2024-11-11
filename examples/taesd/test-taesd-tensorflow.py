c8e926006865edbd47bc85af94f3a5d2eb8ae672
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2024-11-11 12:50:46 +0000

    Add taesd (torch/tensorflow), example scripts and convert script

---
+++ examples/taesd/test-taesd-tensorflow.py
@@ -0,0 +1,74 @@
+"""
+TAESD model from: https://github.com/madebyollin/taesd
+
+Script by Wessel
+"""
+
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import matplotlib.pyplot as plt
+from keras import ops
+
+from usbmd import set_data_paths
+from usbmd.backend.tensorflow.dataloader import h5_dataset_from_directory
+from usbmd.backend.tensorflow.models.taesd import TinyDecoder, TinyEncoder
+from usbmd.utils import get_date_string
+from usbmd.utils.gpu_utils import get_device
+from usbmd.utils.visualize import plot_image_grid
+
+if __name__ == "__main__":
+    # Set up data paths and device
+    data_paths = set_data_paths()
+    data_root = data_paths["data_root"]
+    get_device()
+
+    n_imgs = 10
+    val_dataset = h5_dataset_from_directory(
+        data_root / "USBMD_datasets/CAMUS/val",
+        key="data/image",
+        batch_size=n_imgs,
+        shuffle=True,
+        image_size=[256, 256],
+        resize_type="resize",
+        image_range=[0, 255],
+        normalization_range=[-1, 1],
+    )
+
+    # Get model
+    model_path = "/mnt/z/Ultrasound-BMd/pretrained/taesdxl/wessel-20241107-124716"
+    encoder = TinyEncoder(model_path)
+    decoder = TinyDecoder(model_path)
+
+    batch = next(iter(val_dataset))
+    batch = ops.concatenate([batch, batch, batch], axis=-1)  # grayscale to RGB
+
+    encoded = encoder(batch)
+
+    # NOTE: Here you can compress the encoding a little bit more by going to uint8, like in the original model:
+    # https://github.com/huggingface/diffusers/blob/cd30820/src/diffusers/models/autoencoders/autoencoder_tiny.py?plain=1#L336-L342
+
+    output = decoder(encoded)
+
+    mse = ops.mean((output - batch) ** 2)
+    print("MSE: ", mse.numpy())
+
+    batch = ops.image.rgb_to_grayscale(batch, data_format="channels_last")
+    output = ops.image.rgb_to_grayscale(output, data_format="channels_last")
+
+    batch = ops.squeeze(batch, axis=-1)
+    output = ops.squeeze(output, axis=-1)
+
+    output = (output + 1) / 2
+    batch = (batch + 1) / 2
+
+    plot_list = ops.unstack(batch) + ops.unstack(output)
+    fig, _ = plot_image_grid(plot_list, vmin=0, vmax=1, ncols=n_imgs)
+    fig.savefig(
+        f"test_taesd_{get_date_string()}.png",
+        pad_inches=0.2,
+        bbox_inches="tight",
+        dpi=300,
+    )
+    plt.close()
