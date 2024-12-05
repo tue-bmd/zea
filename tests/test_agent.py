51722b9bfed6303960db973b150049dd727652ff
OisinNolan <oinolan@tcd.ie>
2024-12-05 16:00:50 +0000

    pylint 🔧

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,27 +1,30 @@
-from keras import ops
+"""Test agent functions."""
 
+from keras import ops
 from usbmd.agent.masks import generate_equispaced_lines
 
 
 def test_generate_equispaced_lines():
+    """Test generate_equispaced_lines."""
     expected_lines = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
     lines = generate_equispaced_lines(n_actions=5, n_possible_actions=10)
     assert ops.all(lines == expected_lines)
 
     expected_lines = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
     lines = generate_equispaced_lines(
-        n_actions=5, n_possible_actions=10, previous_actions=lines
+        n_actions=5, n_possible_actions=10, previous_mask=lines
     )
     assert ops.all(lines == expected_lines)
 
     expected_lines = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
     lines = generate_equispaced_lines(
-        n_actions=5, n_possible_actions=10, previous_actions=lines
+        n_actions=5, n_possible_actions=10, previous_mask=lines
     )
     assert ops.all(lines == expected_lines)
 
 
 def test_generate_equispaced_lines_assertion():
+    """Test generate_equispaced_lines assertion."""
     # Should raise AssertionError when n_possible_actions is not divisible by n_actions
     try:
         generate_equispaced_lines(n_actions=3, n_possible_actions=10)
