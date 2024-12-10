d4aa483d27d2d4e4a4ef909ce5ace6d3bc4711d0
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2024-12-10 11:24:12 +0000

    Config object now logs when attributes are used

--- usbmd/config/config.py
+++ usbmd/config/config.py
@@ -16,6 +16,13 @@ import yaml
 from usbmd.utils import log
 
 
+def path_to_str(path):
+    """Convert a Path object to a string."""
+    if isinstance(path, Path):
+        return str(path)
+    return path
+
+
 class Config(dict):
     """Config class.
 
@@ -38,7 +45,7 @@ class Config(dict):
 
     __frozen__ = False
 
-    def __init__(self, dictionary=None, **kwargs):
+    def __init__(self, dictionary=None, __parent__=None, **kwargs):
         """
         Initializes a Config object.
 
@@ -52,9 +59,10 @@ class Config(dict):
         super().__setattr__(
             "__protected__",
             [x[0] for x in inspect.getmembers(Config, predicate=inspect.isroutine)]
-            + ["__protected__"],
+            + ["__protected__", "__accessed__", "__parent__"],
         )
         super().__setattr__("__accessed__", {})
+        super().__setattr__("__parent__", __parent__)
 
         if dictionary is None:
             dictionary = {}
@@ -78,9 +86,12 @@ class Config(dict):
 
         # Ensures lists and tuples of dictionaries are converted to Config objects as well
         if isinstance(value, (list, tuple)):
-            value = [self.__class__(x) if isinstance(x, dict) else x for x in value]
+            value = [
+                self.__class__(x, __parent__=self) if isinstance(x, dict) else x
+                for x in value
+            ]
         elif isinstance(value, dict):
-            value = self.__class__(value)
+            value = self.__class__(value, __parent__=self)
 
         super().__setitem__(name, value)
 
@@ -103,27 +114,47 @@ class Config(dict):
 
     def _mark_accessed(self, name):
         """Mark an attribute as accessed."""
-        super().__setattr__("__accessed__", {name: True})
+        self.__accessed__[name] = True
 
-    def _assert_key_accessed(self, key, value, _assert=True):
+    def _trace_through_ancestors(self, key_trace=None):
+        """Find the root ancestor of the config object."""
+        if key_trace is None:
+            key_trace = []
+        if self.__parent__ is None:
+            return self, key_trace
+        for key, value in self.__parent__.items():
+            if value == self:
+                return self.__parent__._trace_through_ancestors([key] + key_trace)
+
+    @staticmethod
+    def _assert_key_accessed(config, key, value, _assert=True):
         """Assert that a key has been accessed."""
-        if key not in self.__accessed__:
+        if key not in config.__accessed__:
+            key_trace = config._trace_through_ancestors()[1]
             msg = f"Attribute '{key}'='{value}' has not been accessed."
+            if key_trace:
+                msg += f" Has ancestors through '{key_trace}'"
             if _assert:
-                raise AttributeError(msg)
-            else:
-                log.warning(msg)
+                raise AssertionError(msg)
+            log.warning(msg)
         return key, value
 
     def _assert_all_accessed(self):
         """Assert that all attributes have been accessed."""
-        self.as_dict(self._assert_key_accessed, as_config=True)
+        # Temporary remove parent to avoid recursion if not being called from ancestor.
+        self._all_unaccessed(_assert=True)
 
     def _log_all_unaccessed(self):
         """Log all unaccessed attributes."""
-        self.as_dict(
-            lambda k, v: self._assert_key_accessed(k, v, _assert=False), as_config=True
-        )
+        self._all_unaccessed(_assert=False)
+
+    def _all_unaccessed(self, _assert=False):
+        """Assert or log all unaccessed attributes."""
+        # Temporary remove parent to avoid recursion if not being called from ancestor.
+        parent = self.__parent__
+        super().__setattr__("__parent__", None)
+        self.as_dict(lambda *args: self._assert_key_accessed(*args, _assert=_assert))
+        super().__setattr__("__parent__", parent)
 
     def __getattr__(self, name):
         if name in self:
@@ -151,31 +182,39 @@ class Config(dict):
         """Return the config as a json string."""
         return json.dumps(self)
 
-    def as_dict(self, func_on_leaves=None, as_config=False):
+    def as_dict(self, func_on_leaves=None):
         """
         Convert the config to a normal dictionary (recursively).
+
+        Args:
+            func_on_leaves (callable, optional): Function to apply to each leaf node.
+                The function should take three arguments: the config object, the key, and the value.
+                You can change the key and value inside the function. Defaults to None.
         """
-        if as_config:
-            dictionary = Config()
-        else:
-            dictionary = {}
+        dictionary = {}
         for key, value in self.items():
             if isinstance(value, Config):
-                value = value.as_dict()
+                value = value.as_dict(func_on_leaves)
             elif isinstance(value, (list, tuple)):
-                value = [v.as_dict() if isinstance(v, Config) else v for v in value]
+                value = [
+                    v.as_dict(func_on_leaves) if isinstance(v, Config) else v
+                    for v in value
+                ]
             # a dict does not exist inside a Config object, because it is a Config object itself
             if func_on_leaves:
-                key, value = func_on_leaves(key, value)
+                key, value = func_on_leaves(self, key, value)
             dictionary[key] = value
         return dictionary
 
     def serialize(self):
         """Return a dict of this config object with all Path objects converted to strings."""
-        return self.as_dict(lambda k, v: str(v) if isinstance(v, Path) else v)
+        return self.as_dict(lambda _, key, value: (key, path_to_str(value)))
 
     def deep_copy(self):
-        """Deep copy"""
+        """
+        Deep copy the config object. This is useful when you want to modify the config object
+        without changing the original. Does not preserve the access history or frozen state!
+        """
         return Config(copy.deepcopy(self.as_dict()))
 
     def save_to_yaml(self, path):
