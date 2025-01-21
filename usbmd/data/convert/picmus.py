8fec0a7af50b9fdaf8d801df8535d4eae41dcec9
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-01-21 10:03:52 +0000

    Move usbmd/scripts to usbmd/data/convert

--- usbmd/data/convert/picmus.py
+++ usbmd/data/convert/picmus.py
@@ -1,13 +1,25 @@
-"""Functionality to convert the PICMUS dataset to the USBMD format."""
+"""
+Script to convert the PICMUS database to the USBMD format.
 
+Example usage:
+```bash
+python usbmd/data/convert/picmus.py \
+--src_dir /mnt/z/Ultrasound-BMd/data/PICMUS \
+--output_dir converted_PICMUS_dir
+```
+"""
+
+import argparse
 import logging
 import os
+from pathlib import Path
 
 import h5py
 import numpy as np
 
 from usbmd.data.data_format import generate_usbmd_dataset
 from usbmd.scan import compute_t0_delays_planewave
+from usbmd.utils import log
 
 
 def convert_picmus(source_path, output_path, overwrite=False):
@@ -100,3 +112,75 @@ def convert_picmus(source_path, output_path, overwrite=False):
         probe_name="verasonics_l11_4v",
         description="PICMUS dataset converted to USBMD format",
     )
+
+
+def get_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        description=(
+            "Converts the PICMUS database to the USBMD format. The "
+            "src_dir is scanned for hdf5 files ending in iq or rf. These files are"
+            "converted and stored in output_dir under the same relative path as "
+            "they came from in src_dir."
+        )
+    )
+    parser.add_argument(
+        "--src_dir",
+        type=str,
+        help="Source directory where the original PICMUS data is stored.",
+    )
+
+    parser.add_argument(
+        "--output_dir", type=str, help="Output directory of the converted database"
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    # Parse the arguments
+    args = get_args()
+
+    # Get the source and output directories
+    base_dir = Path(args.src_dir)
+    output_dir = Path(args.output_dir)
+
+    # Check if the source directory exists and create the output directory
+    assert base_dir.exists(), f"Source directory {base_dir} does not exist."
+    output_dir.mkdir(parents=True, exist_ok=False)
+
+    # Traverse the source directory and convert all files
+    for file in base_dir.rglob("*.hdf5"):
+        str_file = str(file)
+
+        # Select only the data files that actually contain rf or iq data
+        # (There are also files containing the geometry of the phantoms or
+        # images)
+        if (
+            not str_file.endswith("iq.hdf5") or not str_file.endswith("rf.hdf5")
+        ) and "img" in str_file:
+            log.info("Skipping %s", file.name)
+            continue
+
+        log.info("Converting %s", file.name)
+
+        # Find the folder relative to the base directory to retain the
+        # folder structure in the output directory
+        output_file = output_dir / file.relative_to(base_dir)
+
+        # Define the output path
+        # NOTE: I added output_file.stem to put each file in its own
+        # folder. This makes it possible to use it as a dataset because
+        # it ensures there are never different types of data file in
+        # the same folder.
+        output_file = output_file.parent / output_file.stem / f"{output_file.stem}.hdf5"
+
+        # Convert the file
+        try:
+            # Create the output directory if it does not exist already
+            output_file.parent.mkdir(parents=True, exist_ok=True)
+
+            convert_picmus(file, output_file, overwrite=True)
+        except:
+            output_file.parent.rmdir()
+            log.error("Failed to convert %s", str_file)
+            continue
