3e651ffaf7773a00316f6538f8fc07f495d9f40d
Vincent van de Schaft <vincentvdschaft@gmail.com>
2025-01-29 10:48:32 +0100

    RF simulator implementation

--- usbmd/simulator.py
+++ usbmd/simulator.py
@@ -1,32 +1,103 @@
 from usbmd import Scan
 from keras import ops
 import numpy as np
-
-
-def sinc(x):
-    x = np.pi * x
-    return ops.where(x == 0, 1.0, ops.sin(x) / x)
+from usbmd.utils.lens_correction import compute_lens_corrected_travel_times
 
 
 def simulate_rf(scan: Scan, scat_positions, scat_magnitudes):
 
-    pulse_spectrum_fn = get_pulse_spectrum_fn(scan.fc, n_period=3)
+    pulse_spectrum_fn = get_pulse_spectrum_fn(scan.fc, n_period=4)
 
-    dist = ops.linalg.norm(scan.probe_geometry[None] - scat_positions[:, None], axis=-1)
-    freqs = ops.arange(scan.n_ax // 2 + 1) / scan.n_ax * scan.fs
+    if not scan.apply_lens_correction:
+        dist = ops.linalg.norm(
+            scan.probe_geometry[None] - scat_positions[:, None], axis=-1
+        )
+    else:
+        dist = (
+            compute_lens_corrected_travel_times(
+                scan.probe_geometry,
+                scat_positions,
+                lens_thickness=scan.lens_thickness,
+                c_lens=scan.lens_sound_speed,
+                c_medium=scan.sound_speed,
+                n_iter=3,
+            )
+            * scan.sound_speed
+        )
+
+    freqs = ops.arange(scan.n_ax // 2 + 1) / scan.n_ax * scan.fs + 1
+
     waveform_spectrum = pulse_spectrum_fn(freqs)
     parts = []
     for tx in range(scan.n_tx):
 
-        # [n_scat, n_txel]
-        tau_tx = dist / scan.sound_speed + scan.t0_delays[tx]
-        print(tau_tx.shape)
+        tx_idx = ops.array(tx)
+
+        # [n_scat, n_txel, rxel]
+        dist_total = dist[:, None] + dist[:, :, None]
 
         # [n_scat, n_txel, n_rxel]
-        tau_total = tau_tx[:, :, None] + dist[:, None, :] / scan.sound_speed
+        tau_total = (
+            dist_total / scan.sound_speed
+            + scan.t0_delays[tx_idx]
+            + scan.initial_times[tx_idx]
+        )
 
-        result = waveform_spectrum * delay2(
-            freqs[None, None, None], tau_total[..., None], n_fft=scan.n_ax, fs=scan.fs
+        scat_pos_relative_to_probe = scat_positions[:, None] - scan.probe_geometry[None]
+
+        # Compute 3D directivity
+        theta = ops.arctan2(
+            scat_pos_relative_to_probe[:, :, 0], scat_pos_relative_to_probe[:, :, 2]
+        )
+        phi = ops.arctan2(
+            scat_pos_relative_to_probe[:, :, 1], scat_pos_relative_to_probe[:, :, 2]
+        )
+
+        directivity_tx = directivity(
+            freqs[None, None, None],
+            theta[..., None, None],
+            scan.element_width,
+            scan.sound_speed,
+        ) * directivity(
+            freqs[None, None, None],
+            phi[..., None, None],
+            scan.element_width,
+            scan.sound_speed,
+        )
+        directivity_rx = directivity(
+            freqs[None, None, None],
+            theta[:, None, :, None],
+            scan.element_width,
+            scan.sound_speed,
+        ) * directivity(
+            freqs[None, None, None],
+            phi[:, None, :, None],
+            scan.element_width,
+            scan.sound_speed,
+        )
+
+        attenuation = attenuate(
+            freqs[None, None, None],
+            attenuation_coef=scan.attenuation_coef,
+            dist=dist_total[..., None],
+        )
+
+        spread_atten = spread(dist_total[..., None])
+
+        result = (
+            waveform_spectrum[None, None, None]
+            * scat_magnitudes[:, None, None, None]
+            * scan.tx_apodizations[tx, None, :, None, None]
+            * delay2(
+                freqs[None, None, None],
+                tau_total[..., None],
+                n_fft=scan.n_ax,
+                fs=scan.fs,
+            )
+            * directivity_tx
+            * directivity_rx
+            * attenuation
+            * spread_atten
         )
 
         # Sum over all transmitting elements and scatterers
@@ -45,9 +116,19 @@ def travel(distance, sound_speed, frequency):
     return ops.exp(-1j * distance / sound_speed * frequency)
 
 
-def directivity(theta, frequency, sound_speed):
-    arg = np.pi * sound_speed / frequency * ops.sin(theta)
-    return sinc(arg) * ops.cos(theta)
+# def directivity(theta, frequency, sound_speed):
+#     arg = np.pi * sound_speed / frequency * ops.sin(theta)
+#     return sinc(arg) * ops.cos(theta)
+
+
+def directivity(f, theta, element_width, sound_speed, rigid_baffle=True):
+
+    wavelength = sound_speed / f
+
+    response = sinc(element_width / wavelength * ops.sin(theta))
+    if not rigid_baffle:
+        response *= ops.cos(theta)
+    return response
 
 
 def delay(f, tau):
@@ -193,38 +274,64 @@ def get_transducer_bandwidth_fn(fc, bandwidth):
     return bandwidth_fn
 
 
+def sinc(x):
+    x = ops.abs(np.pi * x) + 1e-9
+    return ops.sin(x) / x
+
+
 if __name__ == "__main__":
     n_el = 128
-    n_scat = 5
-    aperture = 20e-3
+    n_scat = 3
+    n_tx = 2
+
+    aperture = 30e-3
+    from scipy.signal.windows import hann
+
+    tx_apodizations = np.sqrt(hann(n_el)) * np.ones((n_tx, 1))
+    probe_geometry = np.stack(
+        [
+            np.linspace(-aperture / 2, aperture / 2, n_el),
+            np.zeros(n_el),
+            np.zeros(n_el),
+        ],
+        axis=1,
+    )
+
+    # tx_apodizations[:, n_el // 2] = 1.0
     scan = Scan(
-        n_tx=1,
+        n_tx=n_tx,
         n_ax=512,
         n_el=n_el,
         center_frequency=3.125e6,
         sampling_frequency=12.5e6,
-        probe_geometry=ops.stack(
-            [
-                ops.linspace(-aperture / 2, aperture / 2, n_el),
-                ops.zeros(n_el),
-                ops.zeros(n_el),
-            ],
-            axis=1,
-        ),
-        t0_delays=ops.zeros((1, n_el)),
+        probe_geometry=probe_geometry,
+        t0_delays=np.zeros((n_tx, n_el)),
+        tx_apodizations=tx_apodizations,
+        element_width=ops.linalg.norm(probe_geometry[1] - probe_geometry[0]),
+        apply_lens_correction=True,
+        lens_sound_speed=1440,
+        lens_thickness=1e-3,
+        initial_times=np.zeros((n_tx,)),
+        attenuation_coef=0.7,
+    )
+    scat_x, scat_z = np.meshgrid(
+        np.linspace(-10e-3, 10e-3, 5), np.linspace(5e-3, 30e-3, 5), indexing="ij"
+    )
+    scat_x, scat_z = scat_x.flatten(), scat_z.flatten()
+    n_scat = len(scat_x)
+    scat_positions = np.stack(
+        [
+            scat_x,
+            np.zeros_like(scat_x),
+            scat_z,
+        ],
+        axis=1,
     )
 
     rf = simulate_rf(
         scan=scan,
-        scat_positions=ops.stack(
-            [
-                ops.linspace(-10e-3, 10e-3, n_scat),
-                ops.zeros(n_scat),
-                ops.ones(n_scat) * 10e-3,
-            ],
-            axis=1,
-        ),
-        scat_magnitudes=ops.ones(n_scat),
+        scat_positions=scat_positions,
+        scat_magnitudes=np.ones(n_scat),
     )
     import matplotlib.pyplot as plt
 
