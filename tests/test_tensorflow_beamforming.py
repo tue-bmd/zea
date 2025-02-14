c24c32826a387af00cab4abb6c910e1d2ffb29fa
Tristan Stevens <t.s.w.stevens@tue.nl>
2025-02-14 13:11:12 +0000

    simulator tests

--- tests/test_tensorflow_beamforming.py
+++ tests/test_tensorflow_beamforming.py
@@ -16,9 +16,9 @@ from usbmd.backend.tensorflow.layers.beamformers import get_beamformer
 from usbmd.backend.tensorflow.utils.utils import tf_snapshot
 from usbmd.config import load_config_from_yaml
 from usbmd.config.validation import check_config
+from usbmd.ops_v2 import Pipeline, Simulate
 from usbmd.probes import Verasonics_l11_4v
 from usbmd.scan import PlaneWaveScan
-from usbmd.utils.simulator import UltrasoundSimulator
 
 # Add project folder to path to find config files
 wd = Path(__file__).parent.parent
@@ -43,6 +43,8 @@ def test_das_beamforming(
     Returns:
         numpy array: beamformed output
     """
+    # Ensure reproducible results
+    keras.utils.set_random_seed(42)
 
     config = load_config_from_yaml(r"./tests/config_test.yaml")
     config = check_config(config)
@@ -60,7 +62,7 @@ def test_das_beamforming(
         n_tx=1,
         xlims=(-19e-3, 19e-3),
         zlims=(0, 63e-3),
-        n_ax=2047,
+        n_ax=2046,  # TODO: vincent 2047 doesn't work here
         sampling_frequency=probe_parameters["sampling_frequency"],
         center_frequency=probe_parameters["center_frequency"],
         angles=np.array(
@@ -83,20 +85,40 @@ def test_das_beamforming(
     scan.Nx = int(np.ceil((scan.xlims[1] - scan.xlims[0]) / dx))
     scan.Nz = int(np.ceil((scan.zlims[1] - scan.zlims[0]) / dz))
 
-    simulator = UltrasoundSimulator(probe, scan)
+    # use pipeline here so it is easy to propagate the scan parameters
+    simulator = Pipeline(
+        [Simulate(apply_lens_correction=scan.apply_lens_correction, n_ax=scan.n_ax)]
+    )
+
     beamformer = get_beamformer(probe, scan, config)
 
-    # Ensure reproducible results
-    tf.random.set_seed(0)
-    np.random.seed(0)
-
     # Generate pseudorandom input tensor
-    data = simulator.generate(200)
-
-    inputs = np.expand_dims(data[0], axis=(1, -1))
+    # Define scatterers
+    scat_x, scat_z = np.meshgrid(
+        np.linspace(-10e-3, 10e-3, 5),
+        np.linspace(5e-3, 30e-3, 5),
+        indexing="ij",
+    )
+    scat_x, scat_z = np.ravel(scat_x), np.ravel(scat_z)
+    n_scat = len(scat_x)
+    scat_positions = np.stack(
+        [
+            scat_x,
+            np.zeros_like(scat_x),
+            scat_z,
+        ],
+        axis=1,
+    )
+    output = simulator(
+        scan,
+        probe,
+        scatterer_positions=scat_positions.astype(np.float32),
+        scatterer_magnitudes=np.ones(n_scat, dtype=np.float32),
+    )
+    data = output["raw_data"]
 
     # Perform beamforming and convert to numpy array
-    outputs = beamformer(inputs)
+    outputs = beamformer(data)
 
     # plot results
     if debug:
@@ -104,7 +126,8 @@ def test_das_beamforming(
         aspect_ratio = (data[1].shape[1] / data[1].shape[2]) / (
             data[0].shape[1] / data[0].shape[2]
         )
-        axs[0].imshow(np.abs(inputs.squeeze().T), aspect=aspect_ratio)
+        plot_data = keras.ops.convert_to_numpy(data).squeeze()
+        axs[0].imshow(np.abs(plot_data.T), aspect=aspect_ratio)
         axs[0].set_title("RF data")
         axs[1].imshow(np.squeeze(outputs))
         axs[1].set_title("Beamformed")
