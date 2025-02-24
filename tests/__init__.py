dfc93274cbe879bc7c2a9f58e7276e4d0e41cfb5
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-02-24 09:52:03 +0000

    pylint, tensorflow default test backend and spawn > fork

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,7 +7,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 os.environ["JAX_PLATFORMS"] = "cpu"
 
 # Set default backend for tests
-DEFAULT_TEST_BACKEND = "numpy"
+DEFAULT_TEST_BACKEND = "tensorflow"
 os.environ["KERAS_BACKEND"] = DEFAULT_TEST_BACKEND
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+""" This file contains fixtures that are used by all tests in the tests directory. """
+
 import pytest
 
 from . import elp
@@ -5,6 +7,7 @@ from . import elp
 
 @pytest.fixture(scope="session", autouse=True)
 def run_once_after_all_tests():
+    """Fixture to stop workers after all tests have run."""
     yield
     print("Stopping workers")
     elp.stop_workers()
