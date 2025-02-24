898815e8139b690db4bb42eccee952c8d14d00a5
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-02-24 09:52:03 +0000

    pylint, tensorflow default test backend and spawn > fork

--- tests/helpers2.py
+++ tests/helpers2.py
@@ -11,10 +11,12 @@ import decorator
 import jax
 import numpy as np
 
-from usbmd.setup_usbmd import set_backend
-
 
 class EqualityLibsProcessing:
+    """This class is used to run a test function in multiple backends and compare the results.
+    It starts workers for each backend and runs the test function in each worker.
+    The workers are generally started once per test session in the __init__ file."""
+
     def __init__(self):
         self.result_queues = {}
         self.processes = {}
@@ -23,41 +25,44 @@ class EqualityLibsProcessing:
 
     @staticmethod
     def worker(job_queue, result_queue, env, backend, seed):
+        """Worker function to run the test function in a separate process."""
         # setup worker
         os.environ.update(env)
-        set_backend(backend)
+        os.environ["KERAS_BACKEND"] = backend
         import keras  # pylint: disable=import-outside-toplevel
 
         keras.utils.set_random_seed(seed)
 
         # start worker
-        with jax.disable_jit():
-            while True:
-                job = job_queue.get()
-                if job is None:  # Signal to exit
-                    break
+        while True:
+            job = job_queue.get()
+            if job is None:  # Signal to exit
+                break
 
-                try:
-                    job_id, func_blob, args_blob, kwargs_blob = job
-                    func = pickle.loads(func_blob)
-                    args = pickle.loads(args_blob)
-                    kwargs = pickle.loads(kwargs_blob)
+            try:
+                job_id, func_blob, args_blob, kwargs_blob = job
+                func = pickle.loads(func_blob)
+                args = pickle.loads(args_blob)
+                kwargs = pickle.loads(kwargs_blob)
+                with jax.disable_jit():
                     result = func(*args, **kwargs)
-                    if result is not None:
-                        result = np.array(result)
-                    result_queue.put((job_id, result))
-                except Exception as e:
-                    tb = traceback.format_exc()
-                    result_queue.put((job_id, (e, tb)))
+                if result is not None:
+                    result = np.array(result)
+                result_queue.put((job_id, result))
+            except Exception as e:
+                tb = traceback.format_exc()
+                result_queue.put((job_id, (e, tb)))
 
     def start_workers(self, backends, seed=42):
+        """Start workers for the specified backends."""
         env = os.environ.copy()
+        ctx = multiprocessing.get_context("spawn")
         for backend in backends:
-            job_queue = multiprocessing.Queue(maxsize=1)
-            result_queue = multiprocessing.Queue(maxsize=1)
+            job_queue = ctx.Queue(maxsize=1)
+            result_queue = ctx.Queue(maxsize=1)
             self.result_queues[backend] = result_queue
             self.job_queues[backend] = job_queue
-            self.processes[backend] = multiprocessing.Process(
+            self.processes[backend] = ctx.Process(
                 target=self.worker,
                 args=(job_queue, result_queue, env, backend, seed),
                 daemon=True,
@@ -65,8 +70,11 @@ class EqualityLibsProcessing:
             self.processes[backend].start()
 
     def start_func_in_backend(self, func, args, kwargs, backend, job_id):
+        """Start the test function in the specified backend."""
+        # If no worker is running for the backend, start one
         if backend not in self.job_queues:
             self.start_workers([backend])
+        # Put the job in the job queue
         job_queue = self.job_queues[backend]
         job_queue.put(
             (job_id, pickle.dumps(func), pickle.dumps(args), pickle.dumps(kwargs))
@@ -74,6 +82,8 @@ class EqualityLibsProcessing:
 
     @staticmethod
     def collect_results(result_queues, timeout: int = 30):
+        """Collect results from the result queues of the workers.
+        Will wait for all backends to return a result or raise a TimeoutError."""
         results = {}
         job_ids = []
         for backend, result_queue in result_queues.items():
@@ -81,17 +91,17 @@ class EqualityLibsProcessing:
                 job_id, result = result_queue.get(timeout=timeout)
                 job_ids.append(job_id)
                 results[backend] = result
-            except Empty:
+            except Empty as exc:
                 raise TimeoutError(
                     f"Timeout occurred while waiting for results from backend {backend}"
-                )
+                ) from exc
         assert len(set(job_ids)) in [
             0,
             1,
         ], f"Job IDs do not match across backends: {job_ids}"
         for backend, result in results.items():
             if isinstance(result, tuple) and isinstance(result[0], Exception):
-                raise Exception(
+                raise RuntimeError(
                     f"Child process traceback for backend {backend}:\n"
                     + result[1]
                     + "\n"
@@ -99,6 +109,7 @@ class EqualityLibsProcessing:
         return results
 
     def stop_workers(self):
+        """Stop all workers. This should be called at the end of the test session."""
         for job_queue in self.job_queues.values():
             job_queue.put(None)
         for process in self.processes.values():
@@ -109,6 +120,7 @@ class EqualityLibsProcessing:
         self.job_ids = {}
 
     def get_job_id(self, name):
+        """Get a unique job ID for the test function."""
         name = str(name)
         if name not in self.job_ids:
             self.job_ids[name] = 0
