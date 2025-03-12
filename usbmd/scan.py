35290633ac04ad3be50c1bee29f968bef6e5fff7
Tristan Stevens <t.s.w.stevens@tue.nl>
2025-03-12 08:53:29 +0000

    clean up and fix docstring

--- usbmd/scan.py
+++ usbmd/scan.py
@@ -328,11 +328,17 @@ class Scan(Object):
                 f"Got length {len(focus_distances)}."
             )
 
-        if np.abs(np.max(focus_distances))>1 and np.abs(np.max(focus_distances))<1000:
+        if (
+            np.abs(np.max(focus_distances)) > 1
+            and np.abs(np.max(focus_distances)) < 1000
+        ):
             # check if focus distance is not in wavelengths...
-            log.warning("Focal distance in range 1-1000 m. Assuming wavelenghts.")
-            lambda0 = self.sound_speed/self.fc
-            focus_distances = focus_distances*lambda0
+            log.warning(
+                "Focal distance in range 1-1000 m. "
+                "Assuming wavelenghts instead of meters."
+            )
+            lambda0 = self.sound_speed / self.fc
+            focus_distances = focus_distances * lambda0
 
         self._t0_delays = t0_delays
         self._tx_apodizations = tx_apodizations
