6bc6217e00f5cf7551a58e9c81387927293407de
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-03-18 17:00:47 +0000

    WIP

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,14 +1,14 @@
 """Tests for the device module."""
 
-from tests.test_imports import no_ml_lib_import  # pylint: disable=unused-import
+from tests.test_imports import _no_ml_lib_import
 from usbmd.utils.device import init_device
 
 
-def test_init_device_without_ml_libs(no_ml_lib_import):
+def test_init_device_without_ml_libs():
     """
     Test that the init_device function does not import any ML libraries if backend is None.
     This is important because, for example, Jax should not be imported before
     CUDA_VISIBLE_DEVICES is set.
     """
-    # Run the init_device function within the mock_import fixture context
-    init_device(backend=None)
+    with _no_ml_lib_import(allow_keras_backend=False):
+        init_device(backend=None)
