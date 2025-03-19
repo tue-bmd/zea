Only compute pfield when needed and fix Nx and Nz computation in scan

usbmd/scan.py
@@ -10,7 +10,6 @@ from typing import Union
 import matplotlib.pyplot as plt
 import numpy as np
 from keras import ops
-
 from usbmd.core import STATIC, Object
 from usbmd.utils import log
 from usbmd.utils.pfield import compute_pfield
@@ -203,7 +202,6 @@ class Scan(Object):
                 Defaults to None.
             time_to_next_transmit (np.ndarray, float, optional): The time between subsequent
                 transmit events of shape (n_tx*n_frames,). Defaults to None.
-
         """
         super().__init__()
 
@@ -285,6 +283,8 @@ class Scan(Object):
 
         self.selected_transmits = selected_transmits
         self._static_attrs = STATIC
+
+        self._on_request = ["pfield", "flat_pfield"]
 
     def _set_param(self, name, value, dunder=True):
         """Set a parameter value and mark it as set in _set_params if not None.
@@ -573,6 +573,10 @@ class Scan(Object):
         selected. If set to None, then all transmits are used. Defaults to None."""
         return self._selected_transmits
 
+    def reset_pfield(self):
+        """Reset the pfield to None such that it will be recomputed."""
+        self._pfield = None
+
     @selected_transmits.setter
     def selected_transmits(self, value):
         self._selected_transmits = self._select_transmits(value)
@@ -581,7 +585,7 @@ class Scan(Object):
         except ValueError as e:
             log.warning(f"Error checking for aliasing: {e}")
 
-        self._pfield = None  # also trigger update of the pressure fields
+        self.reset_pfield()  # also trigger update of the pressure fields
 
     @property
     def n_tx(self):
@@ -707,6 +711,7 @@ class Scan(Object):
         If None, the number of pixels is calculated based on the
         `pixels_per_wavelength` parameter. See `usbmd.utils.pixel_grid.get_grid`.
         """
+        self.grid  # Ensure grid is initialized
         return int(self._Nx) if self._Nx is not None else None
 
     @Nx.setter
@@ -722,6 +727,7 @@ class Scan(Object):
         If None, the number of pixels is calculated based on the
         `pixels_per_wavelength` parameter. See `usbmd.utils.pixel_grid.get_grid`.
         """
+        self.grid  # Ensure grid is initialized
         return int(self._Nz) if self._Nz is not None else None
 
     @Nz.setter
@@ -783,9 +789,17 @@ class Scan(Object):
     def grid(self):
         """The beamforming grid of shape (Nz, Nx, 3)."""
         if self._grid is None:
-            self._grid = get_grid(self)
+            self._grid = get_grid(
+                self.xlims,
+                self.zlims,
+                self._Nx,
+                self._Nz,
+                self.sound_speed,
+                self.center_frequency,
+                self.pixels_per_wavelength,
+            )
             self._Nz, self._Nx, _ = self._grid.shape
-            self._pfield = None  # also trigger update of the pressure fields
+            self.reset_pfield()  # also trigger update of the pressure fields
 
         return self._grid
 
@@ -800,6 +814,10 @@ class Scan(Object):
         if self.grid is None:
             return None
         return self.grid.reshape(-1, 3)
+
+    def _default_pfield(self):
+        """Default pfield weighting."""
+        return np.ones((self.n_tx, self.Nz, self.Nx))
 
     @property
     def pfield(self):
@@ -816,7 +834,7 @@ class Scan(Object):
                 log.warning("Nx, Nz, or n_tx not set. Cannot compute pfield.")
                 return self._pfield
 
-            self._pfield = np.ones((self.n_tx, self.Nz, self.Nx))
+            self._pfield = self._default_pfield()
         else:
             if self.pfield_kwargs is None:
                 pfield_kwargs = {}