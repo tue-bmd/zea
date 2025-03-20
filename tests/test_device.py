5bc32061a0d7fb2ee4218c8bedf5bd9022b5a4c7
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-03-20 08:22:03 +0000

    Test every combination of device and backend

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,32 +1,22 @@
 """GPU usage testing"""
 
+from itertools import product
+
 import pytest
 
 from usbmd.utils.device import init_device
 
+devices = ["cpu", "gpu:0", "cuda:0", "auto:-1", "auto:1"]
+backends = ["tensorflow", "torch", "jax", "auto", "numpy"]
 
-@pytest.mark.parametrize(
-    "device, backend",
-    [
-        ("cpu", "tensorflow"),
-        ("cpu", "torch"),
-        ("gpu:0", "tensorflow"),
-        ("gpu:0", "torch"),
-        ("cuda:0", "tensorflow"),
-        ("cuda:0", "torch"),
-        ("auto:-1", "tensorflow"),
-        ("auto:-1", "torch"),
-    ],
-)
+
+@pytest.mark.parametrize("device, backend", list(product(devices, backends)))
 def test_init_device(device, backend):
-    """Test gpu usage setting script"""
-    init_device(device=device, backend=backend)
+    """Test device initialization with combinations of device and backend"""
+    init_device(device=device, backend=backend, verbose=False)
 
 
-@pytest.mark.parametrize(
-    "backend",
-    ["tensorflow", "torch", "jax", "auto", "numpy"],
-)
+@pytest.mark.parametrize("backend", backends)
 def test_default_init_device(backend):
     """Test gpu usage setting script with defaults"""
-    init_device(backend=backend)
+    init_device(backend=backend, verbose=False)
