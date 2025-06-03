9c5892dc3f348f0bc9a0ebe5afc5a67939e231d8
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-06-03 11:33:52 +0000

    Improve docs

--- usbmd/agent/__init__.py
+++ usbmd/agent/__init__.py
@@ -3,8 +3,18 @@
 This module provides tools and utilities for agent-based operations within the usbmd framework,
 including mask generation and action selection strategies.
 
-Submodules:
-    - gumbel: Gumbel-softmax and subset operator utilities.
-    - masks: Functions for generating and manipulating line masks.
-    - selection: Action selection strategies for agent-based tasks.
+Example usage of action selection strategies
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. code-block:: python
+
+    agent = usbmd.agent.selection.GreedyEntropy(
+        n_actions=7,
+        n_possible_actions=112,
+        img_width=112,
+        img_height=112,
+        **kwargs,
+    )
+    particles = np.random.rand(10, 112, 112, 1) # 10 posterior samples
+    lines, mask = agent(particles)
 """
