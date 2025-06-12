5238b88a6ef62391477c1e83799e8fd83e9f6add
Tristan Stevens <t.s.w.stevens@tue.nl>
2025-06-12 11:57:35 +0000

    bug fix

--- zea/data/datasets.py
+++ zea/data/datasets.py
@@ -508,7 +508,7 @@ class Dataset(H5FileHandleCache):
     def __enter__(self):
         return self
 
-    def __exit__(self, **kwargs):  # pylint: disable=unused-argument
+    def __exit__(self, exc_type, exc_val, exc_tb):  # pylint-disable=unused-argument
         self.close()
 
 
