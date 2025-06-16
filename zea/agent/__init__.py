4700c8893cea7eca5ed362ea68a3dc6bfc3ce865
Tristan Stevens <t.s.w.stevens@tue.nl>
2025-06-16 21:36:48 +0000

    fix broken links

--- zea/agent/__init__.py
+++ zea/agent/__init__.py
@@ -1,8 +1,8 @@
-"""
-The `agent` subpackage provides tools and utilities for agent-based algorithms within the `zea` framework,
-including mask generation and action selection strategies. The `masks` and `selection` submodules provide
-key functions for implementing intelligent focused transmit selection, such as the _Greedy Entropy Minimization_ algorithm.
-For a practical example, see [Active Perception for Focused Transmit Selection](http://127.0.0.1:8000/notebooks/agent_example.html).
+"""Agent subpackage for closing action-perception loop in ultrasound imaging.
+
+The `agent` subpackage provides tools and utilities for agent-based algorithms within the ``zea`` framework, including mask generation and action selection strategies. See :mod:`zea.agent.masks` and :mod:`zea.agent.selection` for key functions implementing intelligent focused transmit selection, such as the :class:`zea.agent.selection.GreedyEntropy` algorithm.
+
+For a practical example, see :doc:`../notebooks/agent/agent_example`.
 
 Example usage of action selection strategies
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
