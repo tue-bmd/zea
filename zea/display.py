c3c56d504df81f03c06568b0a73281e574ec4ae6
Tristan Stevens <t.s.w.stevens@tue.nl>
2026-04-20 12:09:28 +0200

    rabbit fixes

--- zea/display.py
+++ zea/display.py
@@ -28,7 +28,7 @@ def to_8bit(image, dynamic_range: Union[None, tuple] = None, pillow: bool = True
         [-60, 0] dB, which is a common range for ultrasound images.
 
     .. note::
-        NaN and values in the input image are replaced with the minimum value of the dynamic range
+        NaN values in the input image are replaced with the minimum value of the dynamic range
         before scaling, which ensures that they are represented as black (0) in the output image.
         +/- inf values are replaced with the min and max values of the dynamic range.
 
@@ -53,7 +53,7 @@ def to_8bit(image, dynamic_range: Union[None, tuple] = None, pillow: bool = True
             ...     fill_value=np.nan,
             ... )
             >>> image = zea.display.to_8bit(image, dynamic_range=(-60, 0))
-            >>> image.save("image.png")
+            >>> image.save("image.png")  # DOCTEST: +SKIP
 
     """
     if dynamic_range is None:
