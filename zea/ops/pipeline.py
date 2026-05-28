8a2b9fc7179475569fd198677c979f05aa2efc47
Tristan Stevens <t.s.w.stevens@tue.nl>
2026-05-28 13:33:25 +0000

    not sure why

--- zea/ops/pipeline.py
+++ zea/ops/pipeline.py
@@ -646,11 +646,6 @@ class Pipeline:
                 >>> config.to_yaml("pipeline.yaml")
                 >>> pipeline = Pipeline.from_path("pipeline.yaml")
 
-            .. testcleanup::
-
-                import os
-                os.remove("pipeline.yaml")
-
         """
         config = Config.from_path(file_path)
         return pipeline_from_config(config, **kwargs)
