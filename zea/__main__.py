c836c8601be1b312d9f76b10b77a06c545c6728c
Tristan Stevens <t.s.w.stevens@tue.nl>
2026-06-23 08:52:59 +0000

    refactor bs100bw_to_iq for public use

--- zea/__main__.py
+++ zea/__main__.py
@@ -95,7 +95,7 @@ def main() -> None:
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", DeprecationWarning)
-        args = tyro.cli(SubCmd)  # ty: ignore[no-matching-overload]
+        args = tyro.cli(SubCmd)
 
     from zea.internal.device import init_device
 
